e89c1cff64edd01ad8722f3a3b342e9854a87b3a
Gabriel Peixoto <gabriel.peixoto@usemobile.xyz>
2024-07-13 17:39:43 -0300

    🧩(#3) : Creating address model

--- lib/modules/home/data/models/address/address_model.dart
+++ lib/modules/home/data/models/address/address_model.dart
@@ -1,30 +1,33 @@
 import 'dart:convert';
 
+import 'package:equatable/equatable.dart';
+
 import '../../../../../shared/shared.dart';
 import '../../../domain/entities/entities.dart';
-import '../../../shared.dart';
 
-class AddressModel extends AddressEntity {
+class AddressModel extends Equatable {
+  final String streetName;
+  final String state;
+  final String city;
+  final double latitude;
+  final double longitude;
   const AddressModel(
-      {required super.streetName,
-      required super.city,
-      required super.state,
-      required super.latitude,
-      asdf
-      asdf
-      as
-      asd
-      f
-      required super.longitude});
+      {required this.streetName,
+      required this.state,
+      required this.city,
+      required this.latitude,
+      required this.longitude});
+
+  factory AddressModel.empty() => const AddressModel(city: '', latitude: 0, longitude: 0, state: '', streetName: '');
 
   factory AddressModel.fromMap({required Map<String, dynamic> map}) {
     try {
       return AddressModel(
         streetName: map['streetName'],
-        city: map['city']
-        state:  map['state']
-        latitude:  map['latitude']
-        longitude: map['longitude']
+        city: map['city'],
+        state: map['state'],
+        latitude: map['latitude'],
+        longitude: map['longitude'],
       );
     } on TypeError catch (error, stackTrace) {
       throw ModelError(error: error, stackTrace: stackTrace);
@@ -33,18 +36,38 @@ class AddressModel extends AddressEntity {
 
   factory AddressModel.fromJson({required String json}) {
     try {
-      return json.isEmpty ? const AddressModel(password: '', email: '') : AddressModel.fromMap(map: jsonDecode(json));
+      return json.isEmpty ? AddressModel.empty() : AddressModel.fromMap(map: jsonDecode(json));
     } on FormatException catch (error, stackTrace) {
       throw ModelError(error: error, stackTrace: stackTrace);
     }
   }
 
-  factory AddressModel.fromEntity({required AuthEntity authEntity}) =>
-      AddressModel(password: authEntity.password, email: authEntity.email);
+  factory AddressModel.fromEntity({required AddressEntity addressEntity}) => AddressModel(
+        city: addressEntity.city,
+        state: addressEntity.state,
+        streetName: addressEntity.streetName,
+        latitude: addressEntity.latitude,
+        longitude: addressEntity.longitude,
+      );
 
-  AuthEntity toEntity() => AuthEntity(password: password, email: email);
+  AddressEntity toEntity() => AddressEntity(
+        city: city,
+        state: state,
+        streetName: streetName,
+        latitude: latitude,
+        longitude: longitude,
+      );
 
-  Map<String, dynamic> toMap() => {'email': email, 'password': password};
+  Map<String, dynamic> toMap() => {
+        'city': city,
+        'state': state,
+        'streetName': streetName,
+        'latitude': latitude,
+        'longitude': longitude,
+      };
 
   String toJson() => jsonEncode(toMap());
+
+  @override
+  List<Object?> get props => [city, state, streetName, latitude, longitude];
 }
